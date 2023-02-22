c40fc2c9f51f60f91f6bbbc9458054d73bd91cee
Vitche Research Team Developer <developer@vitche.com>
2023-02-22 02:29:21 +0200

    1. Properly choosing files for the dump.

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const fs = require("fs");
 const archiver = require('archiver');
 
+const {readdir} = fs.promises;
+
+/**
+ * Builds a list of all sub-folders within a given folder.
+ * @param source The given folder.
+ * @returns {Promise<string[]>} The list of sub-folders.
+ */
+const getDirectories = async source =>
+    (await readdir(source, {withFileTypes: true}))
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+
+/**
+ * A class responsible for making and applying
+ * storage folder archived dumps.
+ */
 class Archiver {
 
+    /**
+     * Creates a new instance of the archiver object.
+     * @param _path Database storage folder path.
+     */
     constructor(_path) {
         this.path = _path;
     }
 
+    /**
+     * Creates an archived storage folder dump.
+     * ZLIB compression level is 9.
+     * @returns {Promise<string>} Resulting dump path within the local file system.
+     */
     async archive() {
 
         const targetPath = `${this.path}.zip`;
@@ -17,18 +42,22 @@ class Archiver {
                 level: 9
             }
         });
-
         archive.pipe(output);
 
-        archive.directory(this.path, "/");
+        // List schema (directories within the storage)
+        const schema = await getDirectories(this.path);
+
+        // Explicitly add all sub-folders
+        for (const directory of schema) {
+            archive.directory(`${this.path}${directory}/`, `/${directory}`);
+        }
 
         await archive.finalize();
-
         return targetPath;
     }
 
-    async unarchive() {
-        return "/tmp/";
+    async extract(_archivePath = undefined) {
+        throw new Error("Not implemented");
     }
 }
 
--- test.js
+++ test.js
@@ -2,13 +2,13 @@ const dump = require("./main");
 
 const main = async function () {
 
-    const archiver = new dump.Archiver("/tmp/");
+    const archiver = new dump.Archiver("./storage/");
 
     let token = await archiver.archive();
     console.log(token);
 
-    token = await archiver.unarchive();
-    console.log(token);
+    // token = await archiver.extract("./storage/.zip");
+    // console.log(token);
 };
 
 main().then(() => {
